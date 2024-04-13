9534154a10588f4ecb5ed491819fb28275cc10f5
Neo40 <tron36.cc@gmail.com>
2024-04-12 22:42:03 -0300

    Adição de atributos da classe Veiculo

---
+++ src/com/cesar/entidades/Veiculo.java
@@ -0,0 +1,14 @@
+package com.cesar.entidades;
+
+import java.time.LocalDateTime;
+
+public class Veiculo {
+    //atributos
+    String placa;
+    String marca;
+    String modelo;
+    String fatorEstacionamento;
+    LocalDateTime horaEntrada;
+    LocalDateTime horaSaida;
+    double totalEstacionamento;
+}
