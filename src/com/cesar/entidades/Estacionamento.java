3e846f212b1524280389988a222865b8cbcb45b3
Neo40 <tron36.cc@gmail.com>
2024-04-12 22:33:55 -0300

    Adição de atributos da classe Estacionamento

---
+++ src/com/cesar/entidades/Estacionamento.java
@@ -0,0 +1,10 @@
+package com.cesar.entidades;
+
+public class Estacionamento {
+    //atributos
+    double valorHora;
+    double totalFaturamento;
+    double totalFaturamentoCarro;
+    double totalFatutamentoMoto;
+    double totalFaturamentoUtilitario;
+}
