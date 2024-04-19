c99bfbaf3e2a04862cc78b0a3360e88ddbf67c4a
Neo40 <tron36.cc@gmail.com>
2024-04-19 16:33:50 -0300

    Encapsulando atributos

--- src/com/cesar/entidades/Estacionamento.java
+++ src/com/cesar/entidades/Estacionamento.java
@@ -15,4 +15,44 @@ public class Estacionamento {
         this.totalFatutamentoMoto = totalFatutamentoMoto;
         this.totalFaturamentoUtilitario = totalFaturamentoUtilitario;
     }
+
+    public double getValorHora() {
+        return valorHora;
+    }
+
+    public void setValorHora(double valorHora) {
+        this.valorHora = valorHora;
+    }
+
+    public double getTotalFaturamento() {
+        return totalFaturamento;
+    }
+
+    public void setTotalFaturamento(double totalFaturamento) {
+        this.totalFaturamento = totalFaturamento;
+    }
+
+    public double getTotalFaturamentoCarro() {
+        return totalFaturamentoCarro;
+    }
+
+    public void setTotalFaturamentoCarro(double totalFaturamentoCarro) {
+        this.totalFaturamentoCarro = totalFaturamentoCarro;
+    }
+
+    public double getTotalFatutamentoMoto() {
+        return totalFatutamentoMoto;
+    }
+
+    public void setTotalFatutamentoMoto(double totalFatutamentoMoto) {
+        this.totalFatutamentoMoto = totalFatutamentoMoto;
+    }
+
+    public double getTotalFaturamentoUtilitario() {
+        return totalFaturamentoUtilitario;
+    }
+
+    public void setTotalFaturamentoUtilitario(double totalFaturamentoUtilitario) {
+        this.totalFaturamentoUtilitario = totalFaturamentoUtilitario;
+    }
 }
